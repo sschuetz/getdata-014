0d64e0d3009b2f45a9ca1d5d9e8c7c23d1761c67
Steffen Schütz <stsch01@gmail.com>
2015-05-16 16:06:27 +0200

    download and extract dataset

---
+++ run_analysis.R
@@ -0,0 +1,44 @@
+library(dplyr)
+library(tidyr)
+
+datasetURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+destFilename <- "Dataset.zip"
+datasetFilenames <-
+  c("UCI HAR Dataset/activity_labels.txt", "UCI HAR Dataset/features.txt", 
+    "UCI HAR Dataset/features_info.txt", "UCI HAR Dataset/README.txt", 
+    "UCI HAR Dataset/test/", "UCI HAR Dataset/test/Inertial Signals/", 
+    "UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt", 
+    "UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt", 
+    "UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt", 
+    "UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt", 
+    "UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt", 
+    "UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt", 
+    "UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt", 
+    "UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt", 
+    "UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt", 
+    "UCI HAR Dataset/test/subject_test.txt", "UCI HAR Dataset/test/X_test.txt", 
+    "UCI HAR Dataset/test/y_test.txt", "UCI HAR Dataset/train/", 
+    "UCI HAR Dataset/train/Inertial Signals/", "UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt", 
+    "UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt", 
+    "UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt", 
+    "UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt", 
+    "UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt", 
+    "UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt", 
+    "UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt", 
+    "UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt", 
+    "UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt", 
+    "UCI HAR Dataset/train/subject_train.txt", "UCI HAR Dataset/train/X_train.txt", 
+    "UCI HAR Dataset/train/y_train.txt")
+
+
+download_and_unzip_dataset <- function () {
+  if(!file.exists(destFilename) || !(unzip(destFilename, list = TRUE)$Name == datasetFilenames)) {
+    download.file(datasetURL, destfile = destfileName, method = "curl")
+  }
+  unzip(destFilename, overwrite = TRUE)
+}
+
+if(FALSE %in% file.exists(datasetFilenames)) {
+  stop("missing files")
+}
+
